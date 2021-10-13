e301a54169ce5e56c6e7fa8462c0d03f21e560bb
Juan Galarce <j6alarce@gmail.com>
2021-10-13 11:29:53 -0300

    Se agregan validaciones a los campos y se muestran en pantalla

--- src/App.js
+++ src/App.js
@@ -1,5 +1,23 @@
 import { useFormik } from "formik";
 
+const validate = (values) => {
+  const errors = {};
+
+  if (!values.name) {
+    errors.name = "Requerido";
+  } else if (values.name.length < 5) {
+    errors.name = "El nombre es muy corto";
+  }
+
+  if (!values.lastname) {
+    errors.lastname = "Requerido";
+  } else if (values.lastname.length < 5) {
+    errors.lastname = "El apellido es muy corto";
+  }
+
+  return errors;
+};
+
 function App() {
   const formik = useFormik({
     initialValues: {
@@ -7,6 +25,7 @@ function App() {
       lastname: "",
       email: "",
     },
+    validate,
     onSubmit: (values) => console.log(values),
   });
 
@@ -14,27 +33,37 @@ function App() {
     <form onSubmit={formik.handleSubmit}>
       <label>Nombre</label>
       <input
+        autoComplete='off'
         name='name'
         type='text'
         onChange={formik.handleChange}
         value={formik.values.name}
       />
+      {formik.errors.name ? (
+        <div style={{ color: "red" }}>{formik.errors.name}</div>
+      ) : null}
+      <br />
       <label>Apellido</label>
-
       <input
+        autoComplete='off'
         name='lastname'
         type='text'
         onChange={formik.handleChange}
         value={formik.values.lastname}
       />
+      {formik.errors.lastname ? (
+        <div style={{ color: "red" }}>{formik.errors.lastname}</div>
+      ) : null}
+      <br />
       <label>Email</label>
       <input
+        autoComplete='off'
         name='email'
         type='email'
         onChange={formik.handleChange}
         value={formik.values.email}
       />
-      <button type="submit">Enviar</button>
+      <button type='submit'>Enviar</button>
     </form>
   );
 }
