04222aff5420fb7247964dc97bb612d330ca11f5
Juan Galarce <j6alarce@gmail.com>
2021-10-13 11:14:20 -0300

    Se crea un ejemplo basico y su configuracion con formik

--- src/App.js
+++ src/App.js
@@ -1,9 +1,41 @@
+import { useFormik } from "formik";
 
 function App() {
+  const formik = useFormik({
+    initialValues: {
+      name: "",
+      lastname: "",
+      email: "",
+    },
+    onSubmit: (values) => console.log(values),
+  });
 
   return (
-    <div>
-    </div>
+    <form onSubmit={formik.handleSubmit}>
+      <label>Nombre</label>
+      <input
+        name='name'
+        type='text'
+        onChange={formik.handleChange}
+        value={formik.values.name}
+      />
+      <label>Apellido</label>
+
+      <input
+        name='lastname'
+        type='text'
+        onChange={formik.handleChange}
+        value={formik.values.lastname}
+      />
+      <label>Email</label>
+      <input
+        name='email'
+        type='email'
+        onChange={formik.handleChange}
+        value={formik.values.email}
+      />
+      <button type="submit">Enviar</button>
+    </form>
   );
 }
 
